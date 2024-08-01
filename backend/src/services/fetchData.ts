634586f0829f7950ea6189ae128b4fa71620affa
DeepSK <deepsk3697@gmail.com>
2024-08-01 14:47:56 +0530

    added livecoinwatch cron bot

--- backend/src/services/fetchData.ts
+++ backend/src/services/fetchData.ts
@@ -2,21 +2,35 @@ import axios from 'axios';
 import { Data } from '../models/Data';
 import cron from 'node-cron';
 
-const API_URL = 'https://api.example.com'; // Replace with actual API URL
+const API_URL = 'https://api.livecoinwatch.com/coins/list';
 
 export const fetchData = () => {
   cron.schedule('*/5 * * * * *', async () => {
     try {
-      const response = await axios.get(`${API_URL}/path-to-endpoint`);
-      const { data } = response;
+      const response = await axios.post(API_URL, {
+        currency: "USD",
+        sort: "rank",
+        order: "ascending",
+        offset: 0,
+        limit: 5,
+        meta: false,
+      }, {
+        headers: {
+          "content-type": "application/json",
+          "x-api-key": "b5dfbbe7-ff3e-41d3-85fb-3b4873aad9ec",
+        }
+      });
+
+      const { data } = response; 
+      console.log('data received : ', data);
 
       for (const item of data) {
         const newData = new Data({
-          symbol: item.symbol,
-          price: item.price,
+          symbol: item.code, 
+          price: item.rate.toFixed(4), 
         });
 
-        await newData.save();
+        await newData.save().then(val  => console.log(`${val.symbol} is saved to DB`)).catch(err => console.log('error with saving in DB : ',err));
       }
     } catch (error) {
       console.error('Error fetching data', error);
